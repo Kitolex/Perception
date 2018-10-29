2201fe6dbb1fca14e65fa95908b750ae16e09f9c
Kitolex <kitolex.soluce@gmail.com>
2018-10-28 22:03:54 -0400

    init plus placement certain script

---
+++ Perception/Assets/SCRIPTS/OBJETS/OBJETS_INTERACTIFS/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : ObjetInteractifs
+{
+    public override void Activation()
+    {
+        Debug.Log("BOUTTON ACTIONNER");
+        //TODO : activation boutton
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Perception/Assets/SCRIPTS/OBJETS/OBJETS_INTERACTIFS/InteractionObjet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObjet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO : détecter interaction possible et lancer méthode objetActiver quand c'est ok
+    }
+
+    /**
+     * Méthode qui lance la méthode activation de l'objet interractifs      
+     */ 
+    private void objetActiver()
+    {
+        ObjetInteractifs objet = GetComponent<ObjetInteractifs>();
+        if (objet !=null)
+        {
+            objet.Activation();
+        }
+    }
+}
