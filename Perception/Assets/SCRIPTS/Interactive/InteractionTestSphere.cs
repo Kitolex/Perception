5aaa020ff89b982247450ae1437b7b5886f29ac4
Unknown <leo.posticblu@orange.fr>
2018-10-30 16:11:09 -0400

    Character controller + Interaction

---
+++ Perception/Assets/SCRIPTS/Interactive/InteractionTestSphere.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTestSphere : MonoBehaviour, IInteractive
+{
+    public void Interact()
+    {
+        Debug.Log("Interacted with sphere");
+    }
+}
