5fa8db489ff60db7f342faf6e73142898e5cf195
Fiesta87 <jb@pommeret.fr>
2018-11-04 16:21:59 -0500

    ajout d'un son d'interaction avec le bouton

--- Perception/Assets/SCRIPTS/Interactive/ButtonEnigme.cs
+++ Perception/Assets/SCRIPTS/Interactive/ButtonEnigme.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class ButtonEnigme : MonoBehaviour, IInteractive {
 
     public bool IsSolution;
+    public AudioClip sound;
+
     [HideInInspector]
-    public EnigmeManager enigmeManager; 
+    public EnigmeManager enigmeManager;
+
+    private AudioSource audioSource;
+
+    void Awake () {
+        this.audioSource = GetComponent<AudioSource>();
+    }
 
     public void Interact()
     {
+        this.audioSource.PlayOneShot(sound);
         if (IsSolution)
             enigmeManager.SolveEnigme();
         else
