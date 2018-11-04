740544ff4047780d2727f95054da209555d64c19
Unknown <leo.posticblu@orange.fr>
2018-11-04 12:28:04 -0500

    Enigme 1 v1

---
+++ Perception/Assets/SCRIPTS/Interactive/ButtonEnigme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEnigme : MonoBehaviour, IInteractive {
+
+    public bool IsSolution;
+    [HideInInspector]
+    public EnigmeManager enigmeManager; 
+
+    public void Interact()
+    {
+        if (IsSolution)
+            enigmeManager.SolveEnigme();
+        else
+            enigmeManager.ResetEnigme();
+    }
+}
---
+++ Perception/Assets/SCRIPTS/OBJETS/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private Animator animator;
+    private Collider collid;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        collid = GetComponent<Collider>();
+	}
+	
+	public void OpenDoor()
+    {
+        animator.SetBool("isOpen", true);
+        collid.enabled = false;
+    }
+
+    public void CloseDoor()
+    {
+        animator.SetBool("isOpen", false);
+        collid.enabled = true;
+    }
+}
