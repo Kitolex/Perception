aaaeb10521fbaa486ccdb6c961fe6243aedbdbd5
Fiesta87 <jb@pommeret.fr>
2018-11-07 12:00:36 -0500

    ajout SubtitleManager.

---
+++ Perception/Assets/SCRIPTS/SYSTEMES/NarrativeText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Text", menuName = "Narrative Text", order = 1)]
+public class NarrativeText : ScriptableObject {
+
+	public string title;
+	[TextArea]
+	public string[] displayedTexts;
+}
