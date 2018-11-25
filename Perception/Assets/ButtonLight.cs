8bd884b98fbd8810183cc093b9d788adb194e717
Unknown <leo.posticblu@orange.fr>
2018-11-25 13:42:37 -0500

    Light button + deco level

---
+++ Perception/Assets/ButtonLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLight : MonoBehaviour {
+
+    private Light light;
+
+	void Start () {
+        light = GetComponentInChildren<Light>();
+	}
+	
+	public void ActivateLight()
+    {
+        light.enabled = true;
+    }
+
+    public void DesactivateLight()
+    {
+        light.enabled = false;
+    }
+}
