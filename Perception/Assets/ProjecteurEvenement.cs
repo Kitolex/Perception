dd54c5813f65e40abe47d2d174501aa8abaaf3b7
Kitolex <kitolex.soluce@gmail.com>
2018-12-04 18:32:17 -0500

    test projecteur

---
+++ Perception/Assets/ProjecteurEvenement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjecteurEvenement : Evenement {
+
+    public void flash()
+    {
+        
+        Material mat = GetComponent<Renderer>().materials[2];
+        Debug.Log(mat.name);
+        mat.EnableKeyword("_EMISSION");
+        VisionStateMachine.Instance.ChangeState(VisionStates.FlashVision);
+        
+    }
+
+}
